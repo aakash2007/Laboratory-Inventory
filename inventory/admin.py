9aa74005624c3c12e16912d5a5e532eb1e3b68b3
Aakash Bajaj <aakashbajaj5311@gmail.com>
2017-08-21 17:33:22 +0530

    Working

--- inventory/admin.py
+++ inventory/admin.py
@@ -2,5 +2,24 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+class ItemInline(admin.TabularInline):
+	model = Item
+	extra = 0
+
+class ItemAdmin(admin.ModelAdmin):
+	fields = ['item_name', 'item_count']
+	search_fields = ['item_name']
+	list_display = ['item_name', 'item_count']
+
+class LaboratoryAdmin(admin.ModelAdmin):
+	fields = ['lab_name', 'lab_incharge']
+	inlines = [ItemInline]
+	list_display = ('lab_name', 'lab_incharge')
+	search_fields = ['lab_name']
+
+
+admin.site.register(Laboratory, LaboratoryAdmin)
+admin.site.register(Item, ItemAdmin)
--- inventory/models.py
+++ inventory/models.py
@@ -4,3 +4,21 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Laboratory(models.Model):
+	lab_name = models.CharField("Laboratory Name", max_length=200)
+	lab_incharge = models.CharField("Laboratory Incharge", max_length=200)
+	def __str__(self):
+		return self.lab_name
+
+	class Meta:
+		verbose_name = "Laboratory"
+		verbose_name_plural = "Laboratories"
+
+class Item(models.Model):
+	lab = models.ForeignKey(Laboratory, on_delete=models.CASCADE)
+	item_name = models.CharField("Item Name", max_length=200)
+	item_count = models.IntegerField("Count", default=0)
+	item_cost = models.IntegerField("Amount", default=0)
+	def __str__(self):
+		return self.item_name
