5218d8f0d386bcb1ef8cfb8d5b3bec7368bfcfd2
Aakash Bajaj <aakashbajaj5311@gmail.com>
2017-09-07 21:22:11 +0530

    Added total cost

--- inventory/models.py
+++ inventory/models.py
@@ -19,6 +19,12 @@ class Item(models.Model):
 	lab = models.ForeignKey(Laboratory, on_delete=models.CASCADE)
 	item_name = models.CharField("Item Name", max_length=200)
 	item_count = models.IntegerField("Count", default=0)
-	item_cost = models.IntegerField("Amount", default=0)
+	item_cost = models.DecimalField("Cost (In INR)", max_digits=10, decimal_places=2)
+	total_cost = models.DecimalField("Total Cost (In INR)", max_digits=10, decimal_places=2)
+	def save(self, *args, **kwargs):
+		self.total_cost = self.item_count*self.item_cost
+
+		return super(Item, self).save(*args, **kwargs)
+
 	def __str__(self):
 		return self.item_name
